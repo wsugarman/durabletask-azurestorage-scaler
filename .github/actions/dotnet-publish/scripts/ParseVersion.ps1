a4bf71af2d425598927ef2e42c9019b301126c87
Will Sugarman <will.sugarman@microsoft.com>
2022-01-20 21:01:13 -0500

    Start Helm Chart (#14)
    - Add initial Helm chart
    - Update README
    - Read service versions from helm chart appVersion
    - Add dependabot entry for docker

--- .github/actions/dotnet-publish/scripts/ParseVersion.ps1
+++ .github/actions/dotnet-publish/scripts/ParseVersion.ps1
@@ -2,7 +2,7 @@ param
 (
     [Parameter(Mandatory=$True)]
     [string]
-    $VersionFilePath,
+    $HelmChartPath,
 
     [Parameter(Mandatory=$False)]
     [Nullable[int]]
@@ -13,26 +13,37 @@ param
 Set-PSDebug -Off
 $ErrorActionPreference = "Stop"
 
-$versionParts = Get-Content $VersionFilePath | Out-String | ConvertFrom-Json
-$assemblyVersion = [string]::Format("{0}.0.0.0", $versionParts.major)
+$line = Get-Content $HelmChartPath | Select-String -Pattern '^appVersion: (?<appVersion>.+)$' | Select-Object -First 1
+if (!$line || !$line.Matches.Success)
+{
+    throw [InvalidOperationException]::new("Cannot find appVersion in helm chart '$HelmChartPath'")
+}
+
+$tagVersion = $line.Matches.Groups[1].Value.Trim()
+if (!($tagVersion -match '^(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)(?:-(?<suffix>[a-zA-Z]+\.\d+))?$'))
+{
+    throw [InvalidOperationException]::new("Unexpected helm chart version '$tagVersion'")
+}
+
+$assemblyVersion = [string]::Format("{0}.0.0.0", $Matches.major)
 
 # PR builds indicate that they're unofficial and pre-release via a change to their file and package versions.
 if ($PullRequestNumber)
 {
-    $fileVersion    = [string]::Format("{0}.{1}.{2}.{3}"   , $versionParts.major, $versionParts.minor, $versionParts.patch, $PullRequestNumber)
-    $packageVersion = [string]::Format("{0}.{1}.{2}-pr.{3}", $versionParts.major, $versionParts.minor, $versionParts.patch, $PullRequestNumber)
+    $fileVersion    = [string]::Format("{0}.{1}.{2}.{3}"   , $Matches.major, $Matches.minor, $Matches.patch, $PullRequestNumber)
+    $packageVersion = [string]::Format("{0}.{1}.{2}-pr.{3}", $Matches.major, $Matches.minor, $Matches.patch, $PullRequestNumber)
 }
 else
 {
-    $fileVersion    = [string]::Format("{0}.{1}.{2}.0", $versionParts.major, $versionParts.minor, $versionParts.patch)
-    $packageVersion = [string]::Format("{0}.{1}.{2}"  , $versionParts.major, $versionParts.minor, $versionParts.patch)
-    if (![string]::IsNullOrWhiteSpace($versionParts.suffix))
+    $fileVersion    = [string]::Format("{0}.{1}.{2}.0", $Matches.major, $Matches.minor, $Matches.patch)
+    $packageVersion = [string]::Format("{0}.{1}.{2}"  , $Matches.major, $Matches.minor, $Matches.patch)
+    if (![string]::IsNullOrWhiteSpace($Matches.suffix))
     {
-        $packageVersion += "-" + $versionParts.suffix
+        $packageVersion += "-" + $Matches.suffix
     }
 }
 
 # Output variables to be used in the build
 Write-Host "::set-output name=assembly::$assemblyVersion"
 Write-Host "::set-output name=file::$fileVersion"
-Write-Host "::set-output name=package::$packageVersion"
+Write-Host "::set-output name=tag::$tagVersion"
---
+++ charts/durabletask-azurestorage-external-scaler/templates/_helpers.tpl
@@ -0,0 +1,74 @@
+{{/* vim: set filetype=mustache: */}}
+
+{{/*
+Expand the name of the chart.
+*/}}
+{{- define "durabletask-azurestorage-external-scaler.name" -}}
+{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" }}
+{{- end }}
+
+{{/*
+Create a default fully qualified app name.
+We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
+If release name contains chart name it will be used as a full name.
+*/}}
+{{- define "durabletask-azurestorage-external-scaler.fullname" -}}
+{{- if .Values.fullnameOverride }}
+{{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" }}
+{{- else }}
+{{- $name := default .Chart.Name .Values.nameOverride }}
+{{- if contains $name .Release.Name }}
+{{- .Release.Name | trunc 63 | trimSuffix "-" }}
+{{- else }}
+{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" }}
+{{- end }}
+{{- end }}
+{{- end }}
+
+{{/*
+Create chart name and version as used by the chart label.
+*/}}
+{{- define "durabletask-azurestorage-external-scaler.chart" -}}
+{{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" }}
+{{- end }}
+
+{{/*
+Common labels
+*/}}
+{{- define "durabletask-azurestorage-external-scaler.labels" }}
+helm.sh/chart: {{ include "durabletask-azurestorage-external-scaler.chart" . }}
+{{ include "durabletask-azurestorage-external-scaler.selectorLabels" . }}
+app.kubernetes.io/component: external-scaler
+app.kubernetes.io/managed-by: {{ .Release.Service }}
+app.kubernetes.io/version: {{ .Chart.Version | quote }}
+{{- if .Values.additionalLabels }}
+{{ toYaml .Values.additionalLabels }}
+{{- end }}
+{{- end }}
+
+{{/*
+Selector labels
+*/}}
+{{- define "durabletask-azurestorage-external-scaler.selectorLabels" -}}
+app.kubernetes.io/name: {{ include "durabletask-azurestorage-external-scaler.name" . }}
+app.kubernetes.io/instance: {{ .Release.Name }}
+{{- end }}
+
+{{/*
+Create the name of the service account to use
+*/}}
+{{- define "durabletask-azurestorage-external-scaler.serviceAccountName" -}}
+{{- if .Values.serviceAccount.create }}
+{{- default (include "durabletask-azurestorage-external-scaler.fullname" .) .Values.serviceAccount.name }}
+{{- else }}
+{{- default "default" .Values.serviceAccount.name }}
+{{- end }}
+{{- end }}
+
+{{/*
+Returns a set of matchLabels applied.
+*/}}
+{{- define "durabletask-azurestorage-external-scaler.matchLabels" -}}
+app: {{ template "durabletask-azurestorage-external-scaler.name" . }}
+release: {{ .Release.Name }}
+{{- end -}}
