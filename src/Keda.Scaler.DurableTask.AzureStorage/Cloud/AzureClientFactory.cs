2486db95776ddc01b845928491ade0a87c0fe4f3
Peng Chen <penche@microsoft.com>
2022-01-26 09:46:33 -0800

    merge 2 factories together

--- src/Keda.Scaler.DurableTask.AzureStorage/Cloud/AzureClientFactory.cs
+++ src/Keda.Scaler.DurableTask.AzureStorage/Cloud/AzureClientFactory.cs
@@ -11,13 +11,14 @@ using Microsoft.Extensions.Options;
 
 namespace Keda.Scaler.DurableTask.AzureStorage.Cloud;
 
-internal class BlobClientFactory
+internal class AzureClientFactory
 {
     private readonly BlobClientOptions _blobClientOptions;
-
-    public BlobClientFactory(IOptionsSnapshot<BlobClientOptions> blobClientOptions)
+    private readonly QueueClientOptions _queueClientOptions;
+    public AzureClientFactory(IOptionsSnapshot<BlobClientOptions> blobClientOptions, IOptionsSnapshot<QueueClientOptions> queueClientOptions)
     {
         _blobClientOptions = EnsureArg.IsNotNull(blobClientOptions?.Value, nameof(blobClientOptions));
+        _queueClientOptions = EnsureArg.IsNotNull(queueClientOptions?.Value, nameof(queueClientOptions));
     }
 
     public BlobServiceClient GetBlobServiceClient(string connectionString)
@@ -34,6 +35,22 @@ internal class BlobClientFactory
         // We assume there is only 1 managed identity present in the hosting environment
         TokenCredential credential = new ManagedIdentityCredential(options: new TokenCredentialOptions { AuthorityHost = endpoints.AuthorityHost });
         return new BlobServiceClient(CreateServiceEndpoint("https", accountName, "blob." + endpoints.StorageSuffix), credential, _blobClientOptions);
+    }
+
+    public QueueServiceClient GetQueueServiceClient(string connectionString)
+    {
+        EnsureArg.IsNotEmptyOrWhiteSpace(connectionString, nameof(connectionString));
+        return new QueueServiceClient(connectionString, _queueClientOptions);
+    }
+
+    public QueueServiceClient GetQueueServiceClient(string accountName, CloudEndpoints endpoints)
+    {
+        EnsureArg.IsNotEmptyOrWhiteSpace(accountName, nameof(accountName));
+        EnsureArg.IsNotNull(endpoints, nameof(endpoints));
+
+        // We assume there is only 1 managed identity present in the hosting environment
+        TokenCredential credential = new ManagedIdentityCredential(options: new TokenCredentialOptions { AuthorityHost = endpoints.AuthorityHost });
+        return new QueueServiceClient(CreateServiceEndpoint("https", accountName, "queue." + endpoints.StorageSuffix), credential, _queueClientOptions);
     }
 
     private static Uri CreateServiceEndpoint(string scheme, string accountName, string suffix)
