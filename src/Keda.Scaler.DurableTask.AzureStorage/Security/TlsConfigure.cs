33168bfd016ad1f03f1ed055685ad969ecaba771
Will Sugarman <will.sugarman@microsoft.com>
2023-12-05 14:36:40 -0800

    Kestrel cert passthrough

--- src/Keda.Scaler.DurableTask.AzureStorage/Security/TlsConfigure.cs
+++ src/Keda.Scaler.DurableTask.AzureStorage/Security/TlsConfigure.cs
@@ -45,6 +45,8 @@ internal sealed class TlsConfigure :
         if (_ca is not null)
         {
             options.ClientCertificateMode = ClientCertificateMode.RequireCertificate;
+            options.AllowAnyClientCertificate();
+
             _logger.RequiredClientCertificate();
         }
 
