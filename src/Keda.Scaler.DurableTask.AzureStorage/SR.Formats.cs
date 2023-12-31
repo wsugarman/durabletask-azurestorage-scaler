248342bcf4f8db1f977e81b297d2f298dcc8aedd
Will Sugarman <will.sugarman@microsoft.com>
2023-12-30 22:18:03 -0500

    Prevent Boxing When Logging (#366)

--- src/Keda.Scaler.DurableTask.AzureStorage/SR.Formats.cs
+++ src/Keda.Scaler.DurableTask.AzureStorage/SR.Formats.cs
@@ -1,6 +1,7 @@
 // Copyright © William Sugarman.
 // Licensed under the MIT License.
 
+using System.Globalization;
 using System.Text;
 
 namespace Keda.Scaler.DurableTask.AzureStorage;
@@ -21,4 +22,10 @@ internal static class SRF
 
     /// <inheritdoc cref="SR.UnknownValueFormat"/>
     public static CompositeFormat UnknownValueFormat { get; } = CompositeFormat.Parse(SR.UnknownValueFormat);
+
+    public static string Format<TArg0>(CompositeFormat format, TArg0 arg0)
+        => string.Format(CultureInfo.CurrentCulture, format, arg0);
+
+    public static string Format<TArg0, TArg1>(CompositeFormat format, TArg0 arg0, TArg1 arg1)
+        => string.Format(CultureInfo.CurrentCulture, format, arg0, arg1);
 }
