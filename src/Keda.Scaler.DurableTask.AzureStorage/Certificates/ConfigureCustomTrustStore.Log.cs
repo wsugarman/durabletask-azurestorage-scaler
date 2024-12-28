33ddb910704010cbe3ec13f667e77e622a4581d1
Will Sugarman <will.sugarman@microsoft.com>
2024-12-28 10:41:16 -0500

    Code gen and fixes

--- src/Keda.Scaler.DurableTask.AzureStorage/Certificates/ConfigureCustomTrustStore.Log.cs
+++ src/Keda.Scaler.DurableTask.AzureStorage/Certificates/ConfigureCustomTrustStore.Log.cs
@@ -10,6 +10,6 @@ internal static partial class Log
     [LoggerMessage(
         EventId = 13,
         Level = LogLevel.Information,
-        Message = "The custom CA certificate at '{Path}' has been reloaded at with thumbprint {Thumbprint}.")]
+        Message = "The custom CA certificate at '{Path}' has been reloaded with thumbprint {Thumbprint}.")]
     public static partial void ReloadedCustomCertificateAuthority(this ILogger logger, string path, string thumbprint);
 }
